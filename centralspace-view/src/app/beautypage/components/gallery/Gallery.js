e422b17988832c0ae993505a141217e7ac9b1923
Maksymilian Kaminski <mkaminski@csod.com>
2020-10-09 18:16:22 +0200

    gallery refactoring

--- centralspace-view/src/app/beautypage/components/gallery/Gallery.js
+++ centralspace-view/src/app/beautypage/components/gallery/Gallery.js
@@ -1,55 +1,32 @@
 import React from "react";
 import '../../../../resources/gallery/css/gallery.css';
-import GalleryItems from "./GalleryItems.js"
+import GalleryItems from "./GalleryItems.js";
+import NavCategories from "../other/NavCategories.js";
 
 function Gallery() {
-
   return (
     <>
-      <nav className="nav-extended">
+      <NavCategories
+        categories={[
+          {
+            name: "type",
+            items: [
+              { label: "All", reference: "#all" },
+              { label: "Big Bang", reference: "#overhead" },
+              { label: "Sacred Geometry", reference: "#sintel", active: true },
+            ]
+          },
+          {
+            name: "tag",
+            items: [
+              { label: "All", reference: "#all" },
+              { label: "circle", reference: "#sintel" },
+              { label: "Hexagon", reference: "#overhead", active: true },
+            ]
+          },
 
-        <div className="categories-wrapper">
-          <div className="categories-container pin-top">
-            <ul className="categories container" data-filter="type">
-              <li>
-                <a href="#all">
-                  All
-              </a>
-              </li>
-              <li>
-                <a href="#overhead">
-                  Big Bang
-              </a>
-              </li>
-        
-              <li className="active">
-                <a href="#sintel">
-                  Sacred Geometry
-              </a>
-              </li>
-            </ul>
-            <ul className="categories container" data-filter="tag">
-              <li>
-                <a href="#all">
-                  All
-              </a>
-              </li>
-              <li className="active">
-                <a href="#sintel">
-                  circle
-              </a>
-              </li>
-              <li>
-                <a href="#overhead">
-                  Hexagon
-              </a>
-              </li>
-            </ul>
-    
-          </div>
-        </div>
-
-      </nav>
+        ]}
+      />
 
       <div id="portfolio" className="gray">
         <div className="container">
